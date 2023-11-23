481555dc96485002e2e03ca23c22e043b97c8337
nigelhaim <nigelsebastian21@gmail.com>
2023-11-23 21:00:54 +0800

    Checkout 5

---
+++ Automation/crm-elastic-dag.py
@@ -0,0 +1,24 @@
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from includes.vs_modules.merged_data import merge
+
+#https://www.youtube.com/watch?v=IsWfoXY_Duk
+
+args = {
+    'owner': 'Vincent Stevenson',
+    'start_date': days_ago(1) # make start date in the past
+}
+
+dag = DAG(
+    dag_id='crm-elastic-dag',
+    default_args=args,
+    schedule_interval='2 * * * *' # make this workflow happen every 2 minute
+)
+
+with dag:
+    merge = PythonOperator(
+        task_id='merge',
+        python_callable=merge,
+        # provide_context=True
+    )
---
+++ Automation/includes/vs_modules/merged_data.py
@@ -0,0 +1,16 @@
+def merge():
+    import pandas as pd
+    import numpy as np
+
+    #Import Data
+    print('Reading Data..')
+    df_branch_service = pd.read_parquet('branch_service_formatted_values.parquet')
+    df_customer_transaction = pd.read_parquet('finalcustomerinfo.parquet')
+
+    #Merge Data
+    df_merged = pd.merge(df_customer_transaction, df_branch_service)
+    print('Datasets Merged...')
+
+    #Save Data
+    print('Saving Data...')
+    df_merged.to_parquet('merged_data.parquet')
